0d843da781bcc214682be59805eb5d998c3af0f3
hitorie_swk <hitorie_swk@126.com>
2025-01-08 14:43:48 +0800

    change make variable to avoid references

--- stm32/stm32h743_fanke_eval/make.mk
+++ stm32/stm32h743_fanke_eval/make.mk
@@ -10,21 +10,21 @@ BSP_LIBCONFIG_STM32_HAL_USART := y
 BSP_LIBCONFIG_STM32_HAL_SPI   := y
 BSP_LIBCONFIG_STM32_HAL_BASTIM:= y
 
-BSP_BOARD_CDEFS := STM32H743
-BSP_BOARD_CDEFS += USE_HAL_DRIVER
+BOARD_CDEFS := STM32H743
+BOARD_CDEFS += USE_HAL_DRIVER
 
-BSP_BOARD_CSRCS += board_irq.c
-BSP_BOARD_CSRCS += board_rcc_init.c
-BSP_BOARD_CSRCS += board_usb.c
-BSP_BOARD_CSRCS += board_init.c
-BSP_BOARD_CSRCS += board_bsp.c
+BOARD_CSRCS += board_irq.c
+BOARD_CSRCS += board_rcc_init.c
+BOARD_CSRCS += board_usb.c
+BOARD_CSRCS += board_init.c
+BOARD_CSRCS += board_bsp.c
 
-BSP_BOARD_ASMSOURCES += fankeh7_startup_gcc.s
-BSP_BOARD_LNK_FILE   += fankeh7_lnk_gcc.ld
+BOARD_ASMSOURCES += fankeh7_startup_gcc.s
+BOARD_LNK_FILE   += fankeh7_lnk_gcc.ld
 
-BSP_BOARD_CSRCS = ${addprefix ${BSP_ROOT}/stm32/stm32h743_fanke_eval/,${BSP_BOARD_CSRCS}}
-BSP_BOARD_ASMSOURCES = ${addprefix ${BSP_ROOT}/stm32/stm32h743_fanke_eval/,${BSP_BOARD_ASMSOURCES}}
-BSP_BOARD_LNK_FILE = ${addprefix ${BSP_ROOT}/stm32/stm32h743_fanke_eval/,${BSP_BOARD_LNK_FILE}}
+BSP_BOARD_CSRCS = ${addprefix ${BSP_ROOT}/stm32/stm32h743_fanke_eval/,${BOARD_CSRCS}}
+BSP_BOARD_ASMSOURCES = ${addprefix ${BSP_ROOT}/stm32/stm32h743_fanke_eval/,${BOARD_ASMSOURCES}}
+BSP_BOARD_LNK_FILE = ${addprefix ${BSP_ROOT}/stm32/stm32h743_fanke_eval/,${BOARD_LNK_FILE}}
 
 BSP_BOARD_ENTRY_POINT :=  Reset_Handler
 
