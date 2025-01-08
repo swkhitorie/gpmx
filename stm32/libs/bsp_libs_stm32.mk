c9641e4b3de6f71910d31b4828528cdfc4b67c6f
hitorie_swk <hitorie_swk@126.com>
2025-01-08 14:46:04 +0800

    add prefix of libs make variable

--- stm32/libs/bsp_libs_stm32.mk
+++ stm32/libs/bsp_libs_stm32.mk
@@ -3,28 +3,28 @@
 ifeq (${BSP_LIBCONFIG_STM32_LLDRV},y)
 
 include ${BSP_ABSROOTDIR}/stm32/libs/drivers/Make.defs
-LIB_DRVCINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/drivers/,${LIB_DRVCINCDIRS}}
-LIB_DRVCSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/drivers/,${LIB_DRVCSRCS}}
+BSP_LIB_DRVCINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/drivers/,${LIB_DRVCINCDIRS}}
+BSP_LIB_DRVCSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/drivers/,${LIB_DRVCSRCS}}
 
 endif
 
 ifeq (${BSP_LIBCONFIG_STM32_SERIES},BSP_STM32_F1)
 
 include ${BSP_ABSROOTDIR}/stm32/libs/stm32f1xx_hal/Make.defs
-LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f1xx_hal/,${LIB_DRVCINCDIRS}}
-LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f1xx_hal/,${LIB_DRVCSRCS}}
+BSP_LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f1xx_hal/,${LIB_DRVCINCDIRS}}
+BSP_LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f1xx_hal/,${LIB_DRVCSRCS}}
 
 else ifeq (${BSP_LIBCONFIG_STM32_SERIES},BSP_STM32_F4)
 
 include ${BSP_ABSROOTDIR}/stm32/libs/stm32f4xx_hal/Make.defs
-LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f4xx_hal/,${LIB_DRVCINCDIRS}}
-LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f4xx_hal/,${LIB_DRVCSRCS}}
+BSP_LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f4xx_hal/,${LIB_DRVCINCDIRS}}
+BSP_LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f4xx_hal/,${LIB_DRVCSRCS}}
 
 else ifeq (${BSP_LIBCONFIG_STM32_SERIES},BSP_STM32_H7)
 
 include ${BSP_ABSROOTDIR}/stm32/libs/stm32h7xx_hal/Make.defs
-LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32h7xx_hal/,${LIB_DRVCINCDIRS}}
-LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32h7xx_hal/,${LIB_DRVCSRCS}}
+BSP_LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32h7xx_hal/,${LIB_DRVCINCDIRS}}
+BSP_LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32h7xx_hal/,${LIB_DRVCSRCS}}
 endif
 
-LIB_CINCDIRS += ${BSP_ROOT}/stm32/libs/cmsis
+BSP_LIB_CINCDIRS += ${BSP_ROOT}/stm32/libs/cmsis
