e2e9d8c8a3586170f4950abb568e2b2b14a78915
hitorie_swk <hitorie_swk@126.com>
2025-01-08 15:00:09 +0800

    fix some makelist bug

--- stm32/libs/bsp_libs_stm32.mk
+++ stm32/libs/bsp_libs_stm32.mk
@@ -11,20 +11,20 @@ endif
 ifeq (${BSP_LIBCONFIG_STM32_SERIES},BSP_STM32_F1)
 
 include ${BSP_ABSROOTDIR}/stm32/libs/stm32f1xx_hal/Make.defs
-BSP_LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f1xx_hal/,${LIB_DRVCINCDIRS}}
-BSP_LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f1xx_hal/,${LIB_DRVCSRCS}}
+BSP_LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f1xx_hal/,${LIB_CINCDIRS}}
+BSP_LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f1xx_hal/,${LIB_CSRCS}}
 
 else ifeq (${BSP_LIBCONFIG_STM32_SERIES},BSP_STM32_F4)
 
 include ${BSP_ABSROOTDIR}/stm32/libs/stm32f4xx_hal/Make.defs
-BSP_LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f4xx_hal/,${LIB_DRVCINCDIRS}}
-BSP_LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f4xx_hal/,${LIB_DRVCSRCS}}
+BSP_LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f4xx_hal/,${LIB_CINCDIRS}}
+BSP_LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32f4xx_hal/,${LIB_CSRCS}}
 
 else ifeq (${BSP_LIBCONFIG_STM32_SERIES},BSP_STM32_H7)
 
 include ${BSP_ABSROOTDIR}/stm32/libs/stm32h7xx_hal/Make.defs
-BSP_LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32h7xx_hal/,${LIB_DRVCINCDIRS}}
-BSP_LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32h7xx_hal/,${LIB_DRVCSRCS}}
+BSP_LIB_CINCDIRS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32h7xx_hal/,${LIB_CINCDIRS}}
+BSP_LIB_CSRCS = ${addprefix ${BSP_ROOT}/stm32/libs/stm32h7xx_hal/,${LIB_CSRCS}}
 endif
 
 BSP_LIB_CINCDIRS += ${BSP_ROOT}/stm32/libs/cmsis
