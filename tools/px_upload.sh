70ce6bd77c65116d48a853dcf49a912306591545
hitorie_swk <hitorie_swk@126.com>
2025-04-25 11:17:56 +0800

    [tools] add fmu upload and uorb generate script

---
+++ tools/px_upload.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+EXEDIR=`pwd`
+BASEDIR=$(dirname $0)
+SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
+SYSTYPE=`uname -s`
+
+# $1: fmu bin
+# $2: fmu hardware type
+# $3: serial usb port
+
+FMUBIN=$1
+FMUTYPE=$2
+SERIAL_PORTS=$3
+FMUOUTPUT=$(basename $FMUBIN .bin).px4
+
+if [ $FMUTYPE = "fmuv2" ]; then
+PROTOTYPE=$SCRIPTDIR/fmuv2-firmware.prototype
+fi
+
+if [ $FMUTYPE = "fmuv6c" ]; then
+PROTOTYPE=$SCRIPTDIR/fmuv6c-firmware.prototype
+fi
+
+MKFW=$SCRIPTDIR/px_mkfw.py
+
+python $MKFW \
+    --prototype $PROTOTYPE \
+    --image $FMUBIN \
+    > ./bin/$FMUOUTPUT
+
+python $SCRIPTDIR/px_uploader.py --port $SERIAL_PORTS ./bin/$FMUOUTPUT
+
+# windows:
+# busybox.exe bash .\tools\px_upload.sh .\bin\px4_fmu-v2.bin fmuv6c COM13
+
+# linux:
+# ./tools/px_upload.sh ./bin/px4_fmu-v2.bin fmuv6c /dev/ttyACM0
